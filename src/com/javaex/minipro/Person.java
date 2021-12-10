94899a7f6b5966d1bb95712b16b78eaadf5badf8
youdaun <yjyou960@naver.com>
2021-12-11 01:22:46 +0900

    미니프로젝트(자바)완료

---
+++ src/com/javaex/minipro/Person.java
@@ -0,0 +1,41 @@
+package com.javaex.minipro;
+
+public class Person {
+
+	private String name;
+	private String hp;
+	private String company;
+
+	public Person(String name, String hp, String company) {
+		this.name = name;
+		this.hp = hp;
+		this.company = company;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+	
+
+}
